e4344a45ee50153806323f51b2a670a92d27c9ac
Alvaro <alvher.orellana@gmail.com>
2024-04-27 19:41:15 -0300

    Initial Commit

---
+++ Memorize2024/Memorize2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2024App.swift
+//  Memorize2024
+//
+//  Created by Alvaro Orellana on 27-04-24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
