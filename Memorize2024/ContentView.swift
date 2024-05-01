67ffab6b1ec55ffb3ea2d1416244a36ca2d80aee
Alvaro <alvher.orellana@gmail.com>
2024-05-01 19:35:23 -0300

    Finished game to the point shown in lecture 2. Cards are showing i can add and remove them, they turn around and the buttons disable when i reach a minimum or maximum

--- Memorize2024/ContentView.swift
+++ Memorize2024/ContentView.swift
@@ -9,35 +9,82 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let emojis = ["🍭", "🎃", "💀", "👻", "🍬", "🧟", "🍫"]
+    let emojis = ["🍭", "🎃", "💀", "👻", "🍬", "🧟", "🍫", "🧌", "🦄", "🍑", "🥹", "☠️"]
+    @State var numberOfCards = 10
     
     var body: some View {
-        ForEach(emojis, id: \.self) { emoji in
-            CardView(emoji: emoji, isFaceUp: true)
+        VStack {
+            ScrollView {
+                cards
+            }
+            cardCountAdjusters
         }
+        .padding()
+
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))]) {
+            ForEach(0..<numberOfCards, id: \.self) { index in
+                CardView(emoji: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardDecreaseButton
+            Spacer()
+            cardIncreaseButton
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    private var cardIncreaseButton: some View {
+        cardAdjuster(by: +1, imageName: "rectangle.stack.fill.badge.plus")
+
+    }
+    
+    private var cardDecreaseButton: some View {
+        cardAdjuster(by: -1, imageName: "rectangle.stack.fill.badge.minus")
+
+    }
+    
+    private func cardAdjuster(by offset: Int, imageName: String) -> some View {
+        Button(action: { numberOfCards += offset }) {
+            Image(systemName: imageName)
+        }
+        .disabled(numberOfCards + offset < 0 || numberOfCards + offset > emojis.count)
     }
 }
 
 
 struct CardView: View {
-    
     let emoji: String
-    var isFaceUp: Bool
+    @State var isFaceUp: Bool = true
+    
     
     var body: some View {
-        
-        let base = RoundedRectangle(cornerRadius: 30)
         ZStack {
-            base
-                .stroke(lineWidth: 10)
-                .foregroundColor(.orange)
-            
-            Text(emoji).font(.title)
-           // base.foregroundColor(.white)
+            let base = RoundedRectangle(cornerRadius: 20)
+
+            base.fill(.white)
+            base.stroke(lineWidth: 3)
+            Text(emoji).font(.largeTitle)
+            base.opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
+    
 }
 
 #Preview {
     ContentView()
+    //CardView(emoji: "👁️", isFaceUp: true)
 }
